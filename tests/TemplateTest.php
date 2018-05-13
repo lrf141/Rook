ee7e14082e01b8b55346e7cbfa6203cb3db01528
lrf141 <ghost141.kentyo@gmail.com>
2018-05-13 14:40:52 +0900

    Unitテストを追加

---
+++ tests/TemplateTest.php
@@ -0,0 +1,20 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Lrf141\Rook\Template;
+use Lrf141\Rook\Engine;
+
+class TemplateTest extends TestCase{
+
+    public function testEscape()
+    {
+        $engine = new Engine("template/sample");
+        $template = new Template($engine, "test");
+
+        $escape = $template->escape('"hello,world"');
+        $expect = '&quot;hello,world&quot;';
+
+        $this->assertSame($expect, $escape);
+    }
+
+}
